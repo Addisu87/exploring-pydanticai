5c3c938e5ecb344672a3a49f62da01ea1a75d8ce
Addisu <addisuhaile87@gmail.com>
2025-02-10 18:08:38 +0300

    Update logic of q&a

--- backend/question_answer/ai_q_and_a_graph.py
+++ backend/question_answer/ai_q_and_a_graph.py
@@ -3,10 +3,14 @@ from __future__ import annotations as _annotations
 from dataclasses import dataclass, field
 from typing import Annotated
 
+import logfire
 from pydantic_ai import Agent
 from pydantic_ai.format_as_xml import format_as_xml
 from pydantic_ai.messages import ModelMessage
 from pydantic_graph import BaseNode, Edge, End, Graph, GraphRunContext
+
+# 'if-token-present' means nothing will be sent (and the example will work) if you don't have logfire configured
+logfire.configure(send_to_logfire="if-token-present")
 
 ask_agent = Agent("openai:gpt-4o", result_type=str)
 
@@ -24,21 +28,18 @@ class Ask(BaseNode[QuestionState]):
 
     docstring_notes = True
 
-    async def run(
-        self, ctx: GraphRunContext[QuestionState]
-    ) -> Annotated[Answer, Edge(label="Ask the question")]:
+    async def run(self, ctx: GraphRunContext[QuestionState]) -> Answer:
         result = await ask_agent.run(
             "Ask a simple question with a single correct answer.",
             message_history=ctx.state.ask_agent_messages,
         )
         ctx.state.ask_agent_messages += result.all_messages()
         ctx.state.question = result.data
-        return Answer(result.data)
+        return Answer()
 
 
 @dataclass
 class Answer(BaseNode[QuestionState]):
-    question: str
     answer: str | None = None
 
     async def run(self, ctx: GraphRunContext[QuestionState]) -> Evaluate:
@@ -64,8 +65,9 @@ class Evaluate(BaseNode[QuestionState]):
     answer: str
 
     async def run(
-        self, ctx: GraphRunContext[QuestionState]
-    ) -> Annotated[End[str], Edge(label="success")] | Reprimand:
+        self,
+        ctx: GraphRunContext[QuestionState],
+    ) -> Congratulate | Reprimand:
         assert ctx.state.question is not None
         result = await evaluate_agent.run(
             format_as_xml({"question": ctx.state.question, "answer": self.answer}),
@@ -73,10 +75,20 @@ class Evaluate(BaseNode[QuestionState]):
         )
         ctx.state.evaluate_agent_messages += result.all_messages()
         if result.data.correct:
-            return End(result.data.comment)
-
+            return Congratulate(result.data.comment)
         else:
             return Reprimand(result.data.comment)
+
+
+@dataclass
+class Congratulate(BaseNode[QuestionState, None, None]):
+    comment: str
+
+    async def run(
+        self, ctx: GraphRunContext[QuestionState]
+    ) -> Annotated[End, Edge(label="success")]:
+        print(f"Correct answer! {self.comment}")
+        return End(None)
 
 
 @dataclass
@@ -85,9 +97,13 @@ class Reprimand(BaseNode[QuestionState]):
 
     async def run(self, ctx: GraphRunContext[QuestionState]) -> Ask:
         print(f"Comment: {self.comment}")
+        # > Comment: Vichy is no longer the capital of France.
         ctx.state.question = None
         return Ask()
 
 
-question_graph = Graph(nodes=(Ask, Answer, Evaluate, Reprimand))
+question_graph = Graph(
+    nodes=(Ask, Answer, Evaluate, Congratulate, Reprimand), state_type=QuestionState
+)
+question_graph.mermaid_save("image.png", highlighted_nodes=[Answer])
 question_graph.mermaid_save("image.png", highlighted_nodes=[Answer])
--- backend/question_answer/ai_q_and_a_run.py
+++ backend/question_answer/ai_q_and_a_run.py
@@ -1,37 +1,97 @@
 import asyncio
+import sys
+from pathlib import Path
 
+import logfire
 from ai_q_and_a_graph import Answer, Ask, QuestionState, question_graph
+from devtools import debug  # type: ignore
 from pydantic_graph import End, HistoryStep
-from rich.prompt import Prompt
 
 
-async def main():
+async def run_as_continuous():
     state = QuestionState()
     node = Ask()
-    history = list[HistoryStep[QuestionState]] = []  # type: ignore
+    history: list[HistoryStep[QuestionState, None]] = []
 
-    while True:
-        node = await question_graph.next(node, history, state=state)  # type: ignore
-        if isinstance(node, Answer):
-            node.answer = Prompt.ask(node.question)
-        elif isinstance(node, End):
-            print(f"Correct answer! {node.data}")
-            # > Correct answer! Well done, 1 + 1 = 2
-            print([e.data_snapshot() for e in history])
-            """
-            [
-                Ask(),
-                Answer(question='What is the capital of France?', answer='Vichy'),
-                Evaluate(answer='Vichy'),
-                Reprimand(comment='Vichy is no longer the capital of France.'),
-                Ask(),
-                Answer(question='what is 1 + 1?', answer='2'),
-                Evaluate(answer='2'),
-            ]
-            """
-            return
+    while logfire.span("run questions graph"):
+        while True:
+            node = await question_graph.next(node, history, state=state)  # type: ignore
+            if isinstance(node, End):
+                print(f"Correct answer! {node.data}")
+                # > Correct answer! Well done, 1 + 1 = 2
+                debug([e.data_snapshot() for e in history])
+            elif isinstance(node, Answer):
+                assert state.question
+                node.answer = input(f"{state.question}")
+
+                """
+                [
+                    Ask(),
+                    Answer(question='What is the capital of France?', answer='Vichy'),
+                    Evaluate(answer='Vichy'),
+                    Reprimand(comment='Vichy is no longer the capital of France.'),
+                    Ask(),
+                    Answer(question='what is 1 + 1?', answer='2'),
+                    Evaluate(answer='2'),
+                ]
+                """
     # otherwise just continue
 
 
-if __name__ == "__main___":
-    asyncio.run(main())
+async def run_as_cli(answer: str | None):
+    history_file = Path("question_graph_history.json")
+    history = (
+        question_graph.load_history(history_file.read_bytes())
+        if history_file.exists()
+        else []
+    )
+
+    if history:
+        last = history[-1]
+        assert last.kind == "node", "expected last step to be a node"
+        state = last.state
+        assert answer is not None, "answer is required to continue from history"
+        node = Answer(answer)
+    else:
+        state = QuestionState()
+        node = Ask()
+    debug(state, node)
+
+    with logfire.span("run questions graph"):
+        while True:
+            node = await question_graph.next(node, history, state=state)
+            if isinstance(node, End):
+                debug([e.data_snapshot() for e in history])
+                print("Finished!")
+                break
+            elif isinstance(node, Answer):
+                print(state.question)
+                break
+            # otherwise just continue
+
+    history_file.write_bytes(question_graph.dump_history(history, indent=2))
+
+
+if __name__ == "__main__":
+    try:
+        sub_command = sys.argv[1]
+        assert sub_command in ("continuous", "cli", "mermaid")
+    except (IndexError, AssertionError):
+        print(
+            "Usage:\n"
+            " uv run -m chat.question_graph mermaid\n"
+            "or:\n"
+            " uv run -m chat.question_graph continuous\n"
+            "or:\n"
+            " uv run -m chat.question_graph cli [answer]",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    if sub_command == "mermaid":
+        print(question_graph.mermaid_code(start_node=Ask))
+    elif sub_command == "continuous":
+        asyncio.run(run_as_continuous())
+    else:
+        a = sys.argv[2] if len(sys.argv) > 2 else None
+        asyncio.run(run_as_cli(a))
